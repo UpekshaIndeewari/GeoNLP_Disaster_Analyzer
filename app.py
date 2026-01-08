d9c4757f682cd0141243ead672d1099bb57b1e50
Upeksha Indeewari <indiedirisooriya@gmail.com>
2026-01-08 22:21:54 +0100

    Initial GeoNLP Disaster Analyzer

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# GEONLP/app.py
 from flask import Flask, render_template, request
 import requests
 from bs4 import BeautifulSoup
@@ -19,6 +20,14 @@ geolocator = Nominatim(user_agent="disaster_mapper")
 
 
 # -------------------- NLTK CONFIGURATION --------------------
+
+nltk_data_dir = os.path.join(os.getcwd(), "nltk_data")
+nltk.data.path.append(nltk_data_dir)
+os.makedirs(nltk_data_dir, exist_ok=True)
+
+nltk.download('punkt', download_dir=nltk_data_dir)
+nltk.download('wordnet', download_dir=nltk_data_dir)
+
 
 try:
     nltk.data.find('tokenizers/punkt_tab')
